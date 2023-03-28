406b66e29e3b4e17e3fab4fe5f303ab30de1444d
鍋島 章祐 <job.nnaabbee.programming0106@gmail.com>
2023-03-28 16:39:41 +0000

    first commit 不要なファイルを削除した

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# kmt-front-learn-02
-KMT勉強会用のレポジトリ
+## Getting Started
+
+開発サーバーを起動する:
+
+```bash
+yarn dev
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sample Next App</title>
+        <meta name="description" content="kmt-learning-client-app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Hello, World!!</h1>
+      </main>
+    </>
+  )
+}
